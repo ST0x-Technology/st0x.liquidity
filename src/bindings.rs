2f7711ed5cf75e9532ec319b2d42db1d4105e39a
0xgleb <gleb.dianov@protonmail.com>
2025-11-26 18:01:03 -0300

    update bindings

--- src/bindings.rs
+++ src/bindings.rs
@@ -3,13 +3,27 @@ use alloy::sol;
 sol!(
     #![sol(all_derives = true, rpc)]
     #[derive(serde::Serialize, serde::Deserialize)]
-    IOrderBookV4, "lib/rain.orderbook.interface/out/IOrderBookV4.sol/IOrderBookV4.json"
+    IOrderBookV4, "lib/rain.orderbook/out/IOrderBookV4.sol/IOrderBookV4.json"
 );
 
 sol!(
     #![sol(all_derives = true, rpc)]
     #[derive(serde::Serialize, serde::Deserialize)]
     IERC20, "lib/forge-std/out/IERC20.sol/IERC20.json"
+);
+
+#[cfg(test)]
+sol!(
+    #![sol(all_derives = true, rpc)]
+    #[derive(serde::Serialize, serde::Deserialize)]
+    TestERC20, "lib/rain.orderbook/out/ArbTest.sol/Token.json"
+);
+
+#[cfg(test)]
+sol!(
+    #![sol(all_derives = true, rpc)]
+    #[derive(serde::Serialize, serde::Deserialize)]
+    OrderBook, "lib/rain.orderbook/out/OrderBook.sol/OrderBook.json"
 );
 
 sol!(
