4726c009c24fa8dcb63c62dd7aa6c543a6450958
0xgleb <gleb.dianov@protonmail.com>
2026-02-10 22:35:44 +0400

    reconfigure

--- config/secrets.nix
+++
@@ -1,7 +0,0 @@
-let inherit (import ../keys.nix) roles;
-in {
-  "server-schwab.toml.age".publicKeys = roles.service;
-  "server-alpaca.toml.age".publicKeys = roles.service;
-  "reporter-schwab.toml.age".publicKeys = roles.service;
-  "reporter-alpaca.toml.age".publicKeys = roles.service;
-}
