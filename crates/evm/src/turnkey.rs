4eac6131d4648e486c41ae34764100304bea2c1d
0xgleb <39841057+0xgleb@users.noreply.github.com>
2026-07-15 14:24:34 -0300

    fix: upgrade Turnkey SDK to 0.7.0

--- crates/evm/src/turnkey.rs
+++ crates/evm/src/turnkey.rs
@@ -729,6 +729,7 @@ impl TracingTurnkeyClient {
             timestamp_ms: timestamp_ms.to_string(),
             parameters: Some(params),
             organization_id,
+            generate_app_proofs: None,
         };
         let activity = self
             .process_activity(&request, "/public/v1/submit/sign_transaction")
@@ -760,6 +761,7 @@ impl TracingTurnkeyClient {
             timestamp_ms: timestamp_ms.to_string(),
             parameters: Some(params),
             organization_id,
+            generate_app_proofs: None,
         };
         let activity = self
             .process_activity(&request, "/public/v1/submit/sign_raw_payload")
@@ -2081,7 +2083,8 @@ mod tests {
                             "signWith": address.to_string(),
                             "unsignedTransaction": hex::encode(&expected_unsigned_rlp),
                             "type": "TRANSACTION_TYPE_ETHEREUM",
-                        }
+                        },
+                        "generateAppProofs": null
                     })
                     .to_string(),
                 );
